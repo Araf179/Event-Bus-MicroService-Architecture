533a4e7f1e0b051d32d12e5ee02db67e7f0fd612
your name <you@example.com>
2021-05-07 00:01:36 -0400

    Added kubernetes yaml files

--- event-bus/index.js
+++ event-bus/index.js
@@ -15,10 +15,10 @@ app.post('/events', (req, res) => {
     events.push(event);
 
     console.log('sending post')
-    axios.post('http://localhost:4000/events', event);
-    axios.post('http://localhost:4001/events', event);
-    axios.post('http://localhost:4002/events', event);
-    axios.post('http://localhost:4003/events', event);
+    axios.post('http://posts-clusterip-srv:4000/events', event);
+    //axios.post('http://localhost:4001/events', event);
+    //axios.post('http://localhost:4002/events', event);
+    //axios.post('http://localhost:4003/events', event);
     res.send({status: 'ok'});
 });
 
